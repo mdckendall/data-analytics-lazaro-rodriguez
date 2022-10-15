d5b238076d13a4706875af15ed2795f6ab8c979c
lazaro20 <113481833+lazaro-rodriguez@users.noreply.github.com>
2022-10-15 22:24:12 +0000

    fix v3

--- Main.java
+++ Main.java
@@ -22,7 +22,7 @@ System.out.println("Press 1 to learn about salary.");
 System.out.println("Press 2 to learn about the job.");
 System.out.println("Press 3 to learn about demand.");
 System.out.println("Press 4 to view current students.");
-System.out.println("Press 5 quit.");
+System.out.println("Press 5 to quit.");
   choiceentry = input.nextInt();
 
   if(choiceentry == 1){
